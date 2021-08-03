be196c1f4cefcba2816183f1772973f65c90f225
Hyeonchang Shin <shinhc5@gmail.com>
2021-08-03 17:34:03 +0900

    feat: add divide

--- calculator.js
+++ calculator.js
@@ -1,1 +1,2 @@
 exports.multiplyNumber = (a, b) => a * b;
+exports.divideNumber = (a, b) => a / b;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { multiplyNumber } = require('./minus');
+const { multiplyNumber } = require('./calculator');
 
 exports.addNumber = (a, b) => a + b;
 exports.multiplyNumber = multiplyNumber;
